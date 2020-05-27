6a47f90d7f97a079010829d37d9ee5ddbd291b3d
Henry Ho <henry.ho@amd.com>
2020-05-26 22:34:20 -0400

    LOCAL: performance analyze config

--- Tensile/KernelWriter.py
+++ Tensile/KernelWriter.py
@@ -1017,6 +1017,9 @@ class KernelWriter(metaclass=abc.ABCMeta):
       kl.append(self.comment1("local read addresses: init pointers b"))
       kl.append(self.localReadInitPointers(kernel, tensorParametersB))
 
+    if self.enable["executeToInitEnd"]:
+        kl.append(self.functionEnd(kernel, False))
+
     ####################################
     # prefetch: unrolled loop prefix
     ####################################
@@ -1123,6 +1126,8 @@ class KernelWriter(metaclass=abc.ABCMeta):
           self.vgprPool.checkIn(item.tempVgpr)
           item.tempVgpr = None
       pack[pIdx] = Code.Module()
+    if self.enable["executeToLoopEnd"] and isOptNLL:
+      kl.append(self.functionEnd(kernel, False))
     kl.append(self.closeSumAtLeastUnroll(kernel, prefetch=False, isOptNLL=isOptNLL))
 
     return kl
@@ -1263,6 +1268,9 @@ class KernelWriter(metaclass=abc.ABCMeta):
                   kl.append(self.comment("local read inc b"))
                   kl.append(self.localReadInc(kernel, iui, tensorParametersB))
       kl.append(self.closeSumAtLeastUnroll(kernel, prefetch=True, isOptNLL=False))
+
+    if self.enable["executeToPrefetchEnd"]:
+      kl.append(self.functionEnd(kernel,False))
 
     # open unrolled summation loop
     kl.append(self.comment3("Unrolled Loop(s) - Begin"))
@@ -1811,6 +1819,9 @@ class KernelWriter(metaclass=abc.ABCMeta):
       # tail: close
       kl.append(self.closeLoop(kernel, -1, True))
 
+    if self.enable["executeToLoopEnd"]:
+      kl.append(self.functionEnd(kernel, False))
+
     # extra summation loops: global increment and close
     for i in reversed(range(self.otherSummationLoops)):
       kl.append(self.comment("global read inc AB"))
@@ -2025,6 +2036,11 @@ class KernelWriter(metaclass=abc.ABCMeta):
     self.enable["Sync"]           = True and not (dkp>0 and dkp >= 5) and not dkp == -5
     self.enable["MAC"]            = True and not (dkp>0 and dkp >= 6) and not dkp == -6
     self.enable["PostLoop"]       = True and not (dkp>0 and dkp >= 1) and not dkp == -1
+
+    # For performance analyze
+    self.enable["executeToInitEnd"]       = 0
+    self.enable["executeToPrefetchEnd"]   = 0
+    self.enable["executeToLoopEnd"]       = 0
 
     #if dkp:
     #  print "\nKernelWriter enable:", self.enable
