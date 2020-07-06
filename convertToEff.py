639469c2b996200d348d0df306e278035d2ccc11
Ethan Lin <Yi-Chen.Lin@amd.com>
2020-07-06 02:02:21 -0400

    tuned data

--- convertToEff.py
+++ convertToEff.py
@@ -3,9 +3,14 @@ import os
 import sys
 
 #sys.argv[1] = a directory to start the search
-#sys.argv[2] = string to search for in filename
-#sys.argv[3] = sclk freq
-#sys.argv[4] = 0 is for legacy, 1 is for MFMA
+#sys.argv[2] = a directory for output
+#sys.argv[3] = string to search for in filename (architecture)
+#sys.argv[4] = sclk freq
+
+def ensurePath(path):
+  if not os.path.exists(path):
+    os.makedirs(path)
+  return path
 
 def allFiles(startDir):
     current = os.listdir(startDir)
@@ -18,44 +23,19 @@ def allFiles(startDir):
             files.append(fullPath)
     return files
 
-# def calculatePeak(filename,frequency):
-#     cuMap = {"arcturus":120,"vega10":64,"vega20":64}
-#     for key in cuMap:
-#         if key in filename:
-#             numCu = cuMap[key]
-#             print("numCu: ",numCu)
-#     typeMap = {"H":4,"S":2,"D":1}
-#     mfmaMap = {"H":4,"S":2,"D":1}
-#     for key in typeMap:
-#         if key in filename:
-#             multiplier = typeMap[key]
-#             if "arcturus" in filename:
-#                 multiplier = multiplier*mfmaMap[key]
-#             print("multiplier: ",multiplier)
-#     peakGflops = 64*multiplier*numCu*frequency/1000
-#     print("peakGFlops:",peakGflops) 
-#     return peakGflops
-	
-def getArchPeakGFlopsBase(arch,frequency):
+def getArchPeakGFlopsBase(archName,frequency):
     cuMap = {"arcturus":120,"vega10":64,"vega20":64}
-	numCu = cuMap[arch]    
-	peakGflops = 64*numCu*frequency/1000	
-	print("numCu: ",numCu)
-    print("arch PeakGFlops Base:",peakGflops) 
-    return peakGflops	
-	
-def getDataTypeMultiplier(dataType):
-    # BF is not supported in non-MFMA, for MFMA, treat it the same as H 
-    typeMap = {"H":4,"BF":4,"S":2,"D":1}
-	multiplier = typeMap[dataType] if dataType in typeMap else 1
-	print("type multiplier: ",multiplier)
-	return multiplier
-	
-def getMFMAMultiplier(arch,isMFMA,dataType):
-    mfmaMap = {"H":4,"BF":2,"S":1}
-	multiplier = mfmaMap[dataType] if arch == 'arcturus' and isMFMA and dataType in mfmaMap else 1
-    print("mfma multiplier: ",multiplier)
-	return multiplier
+    numCu = cuMap[archName]
+    peakGflops = 64*numCu*frequency/1000
+    print("Num Cu: ",numCu)
+    # print("Arch PeakGFlops: {:f}. (Will multiply Type/MFMA scalar) ".format(peakGflops))
+    return peakGflops
+
+def getMultiplier(archName,isMFMA,dataType):
+    typeMap = {"H":4,"S":2,"D":1}       # BF is not supported
+    mfmaMap = {"H":16,"BF":8,"S":4}     # D is not supported
+    multiplier = mfmaMap[dataType] if isMFMA else typeMap[dataType]
+    return multiplier
 
 def getDataType(typeValue):
     if typeValue == 0:
@@ -66,35 +46,36 @@ def getDataType(typeValue):
         return "H"
     if typeValue == 7:
         return "BF"
-		
-files = allFiles(sys.argv[1])
-architect = sys.argv[2]
-isForMFMA = sys.argv[4]
-peakGFlopsBase = getArchPeakGFlopsBase(architect, sys.argv[3])
 
+print('Convert for Architecture: {:s} with SCLK = {:s}MHz'.format(sys.argv[3], sys.argv[4]) )
+
+startFolder = sys.argv[1]
+outFolder = ensurePath(sys.argv[2])
+arch = sys.argv[3]
+peakGFlopsBase = getArchPeakGFlopsBase(arch, int(sys.argv[4]))
+
+files = allFiles(startFolder)
 for f in files:
-    if architect in f:
+    if arch in f:
         print(f)
         with open(f) as y:
             data = yaml.safe_load(y)
-			dataType = getDataType(data[4]['DataType'])
-			typeMult = getDataTypeMultiplier(dataType)
-			mfmaMult = getMFMAMultiplier(architect, isForMFMA, dataType)
-			peakGFlops = peakGFlopsBase*typeMult*mfmaMult
-			print("PeakGFlops:",peakGflops) 
+            dataType = getDataType(data[4]['DataType'])
             for i in range(0,len(data[7])):
+                # get the kernel ID of this problem
+                kernelID = data[7][i][1][0]
+                # check if this kernel is MFMA (since it's possible that one merged yaml is mixed with mfma and legacy)
+                isForMFMA = ('EnableMatrixInstruction' in data[5][kernelID]) and (data[5][kernelID]['EnableMatrixInstruction'] == True)
+                multiplier = getMultiplier(arch, isForMFMA, dataType)
+                peakGFlops = peakGFlopsBase*multiplier
+                # print("PeakGFlops:",peakGFlops) 
                 data[7][i][1][1] = round(100*data[7][i][1][1]/peakGFlops,3)
-                print(data[7][i][1][1])
-        with open(f, "w") as y:
+                if data[7][i][1][1] > 100:
+                    print("Efficiency {:f} exceeds 100, please check kernel {:d}:".format(data[7][i][1][1], kernelID))  
+                # print(data[7][i][1][1])
+        # setup output folder
+        relativePath = os.path.relpath(f,startFolder)
+        outFullpath = os.path.join(outFolder,relativePath)
+        ensurePath(os.path.dirname(outFullpath))        
+        with open(outFullpath, "w") as y:
             yaml.safe_dump(data,y,default_flow_style=None)
-			
-# for f in files:
-#     if sys.argv[2] in f:
-#         print(f)
-#         with open(f) as y:
-#             data = yaml.safe_load(y)
-#             for i in range(0,len(data[7])):
-#                 data[7][i][1][1] = round(100*data[7][i][1][1]/calculatePeak(f,int(sys.argv[3])),3)
-#                 print(data[7][i][1][1])
-#         with open(f, "w") as y:
-#             yaml.safe_dump(data,y,default_flow_style=None)
